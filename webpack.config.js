730edbfd8067b505d8303d3fc0ca3af6cd6facbc
PeterChen <747853229@qq.com>
2018-04-18 22:18:24 +0800

    Add js Add css

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path')
+
+module.exports = {
+  // 项目入口文件
+  entry: './main.js',
+  output: {
+    // 打包文件地址
+    filename: 'bundle.js',
+    // 项目输出文件地址
+    path: path.resolve(__dirname, './dist')
+  },
+  module: {
+    rules: [
+      {
+        // 正则匹配所有css文件
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader?minimize']
+      }
+    ]
+  }
+}
