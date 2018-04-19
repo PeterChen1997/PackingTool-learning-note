7cc62fe3e92564609f736f0fce2f39503cf181a8
PeterChen <747853229@qq.com>
2018-04-19 13:49:51 +0800

    Add es6

---
+++ ES6/babel介绍.md
@@ -0,0 +1,13 @@
+# babelrc配置
+
+## Plugins
+
+### babel-plugin-transform-runtime
+
+Babel官方提供插件，作用时间减少冗余代码
+
+例如将辅助函数抽取出来，减小文件体积
+
+## Presets
+
+presets属性告诉了要转换的源码使用了那些新的语法
---
+++ ES6/webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: ['babel-loader'],
+      },
+    ]
+  },
+  // 输出 source-map 方便直接调试 ES6 源码
+  devtool: 'source-map'
+};
